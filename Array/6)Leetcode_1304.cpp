5b7341faf487117f157d608aaeea8134be9198a2
nitinkaplas <nitin.kaplas1@tcs.com>
2023-08-10 00:29:33 +0530

    add

---
+++ Array/6)Leetcode_1304.cpp
@@ -0,0 +1,35 @@
+Steps:
+
+Case-1 : If N is even 
+Then run a loop as i=1 to i=n/2 where we add a value as i,-i.
+
+Case-2 : If N is odd.
+THen make n as n=n-1 and run a loop i=1 to i=n/2 and add 0 into it.
+
+
+Solution -:
+vector<int> sumZero(int n) {
+        vector<int>v;
+        int i=1;
+        if(n%2==0)
+        {
+           for(int i=1;i<=n/2;i++)
+           {
+             v.push_back(i);
+             v.push_back(-i);
+           }
+        }
+        else
+        {
+          n=n-1;
+          for(int i=1;i<=n/2;i++)
+          {
+             v.push_back(i);
+             v.push_back(-i);
+          }
+          v.push_back(0);
+        }
+        return v;
+    }
+
+Time Complexity -O(N)
