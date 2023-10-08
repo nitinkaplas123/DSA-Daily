a4ca448e646b1b7d5a347e9158c6308edfd80a86
unknown <nitinkaplas90643@gmail.com>
2023-10-08 20:01:32 +0100

    add

---
+++ Level_2/Binary_Tree/4)postorder_traversal.cpp
@@ -0,0 +1,22 @@
+Postorder->  left,root,right
+
+Solution 1-:
+Using recursion
+
+Code-:
+void helper(TreeNode* root,vector<int>&v)
+{
+        if(root==NULL) return ;
+        helper(root->left,v);
+        helper(root->right,v);
+        v.push_back(root->val);
+}
+
+vector<int> postorderTraversal(TreeNode* root) 
+{
+        vector<int>v;
+        helper(root,v);
+        return v;
+}
+
+
