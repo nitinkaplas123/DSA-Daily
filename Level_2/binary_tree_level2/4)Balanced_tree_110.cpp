9588456e4b689c87b339ea3552e10221c8b21d97
unknown <nitinkaplas90643@gmail.com>
2023-10-09 00:11:43 +0100

    add

---
+++ Level_2/binary_tree_level2/4)Balanced_tree_110.cpp
@@ -0,0 +1,48 @@
+Solution 1-:
+Steps-:
+1)if root is null || root is leaf node that is called balanced binary tree.
+2)Find height at every step left and right of root node.
+3)if(abs(left-right)<=1 and check left side and check right side)
+
+
+Code-: Time->O(n2)
+int height(TreeNode* root)
+    {
+        if(root==NULL) return 0;
+        return 1+max(height(root->left),height(root->right));
+    }
+    bool isBalanced(TreeNode* root) {
+        if(root==NULL) return 1;
+        if(root->left==NULL and root->right==NULL) return 1;
+        int left=height(root->left);
+        int right=height(root->right);
+        return (abs(left-right)<=1 and isBalanced(root->left) and isBalanced(root->right));
+    }
+
+
+Solution 2-:
+Steps-:
+1)Going from bottom to top because we have to return height also 
+2)Intially we make ans=1 if abs(left-right)>1  ans=0;
+
+Code-:
+int helper(TreeNode* root,int &flag)
+{
+        if(root==NULL) return 0;
+        if(root->left==NULL and root->right==NULL) return 1;
+        
+        int left=helper(root->left,flag);
+        int right=helper(root->right,flag);
+
+        if(abs(left-right)>1) 
+        flag=0;
+        
+        return 1+max(left,right);
+}
+
+bool isBalanced(TreeNode* root) 
+{
+        int flag=1;
+        helper(root,flag);
+        return flag;
+}
