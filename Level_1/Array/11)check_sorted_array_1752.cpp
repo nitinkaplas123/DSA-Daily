8feb24ac136596042b42e506bb08402f49a214bc
unknown <nitinkaplas90643@gmail.com>
2023-11-04 17:06:20 +0000

    add

--- Level_1/Array/11)check_sorted_array_1752.cpp
+++ Level_1/Array/11)check_sorted_array_1752.cpp
@@ -39,3 +39,41 @@ bool check(vector<int>& nums)
         }
         return (sorted(nums));
 }
+
+
+
+Solution 2-:
+Steps-:
+1)We make a count variable for if(nums[i-1]>nums[i]).
+2)There is 3 cases-:
+ 
+ 1. If its already sorted then count=0 here we have to return true.
+ 2. The array is sorted rotated like [3,4,5,1,2]  means always arr[0]>arr[n-1]
+    here we use count variable 
+    if its sorted rotated then its count=1
+
+    There is 2 sub cases-:
+    1. [2,1,3,4] here count=1 but here arr[0]<arr[3] means we cannot sort this return false.
+    2. [3,4,5,1,2]  here count=1 and arr[0]>arr[n-1]
+    3. [6,10,6]     here we have to return true arr[0]>=arr[n-1]
+
+ 3. When count is greater than 1 [5,1,2,1,3]
+    return false.
+
+
+
+Code-:
+bool check(vector<int>& nums) 
+{
+        int n=nums.size();
+        int count=0;
+        for(int i=1;i<n;i++)
+        {
+            if(nums[i]<nums[i-1])
+            count++;
+        }
+        if(count==0) return true;
+        else if(count>1) return false;
+        else
+        return (nums[n-1]<=nums[0]);
+}
