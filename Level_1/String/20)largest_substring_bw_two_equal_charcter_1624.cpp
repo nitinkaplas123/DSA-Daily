a2a582b7c297b50742e66c47ed66975a13df3f76
unknown <nitinkaplas90643@gmail.com>
2023-12-31 23:15:52 +0000

    add

--- Level_1/String/20)largest_substring_bw_two_equal_charcter_1624.cpp
+++ Level_1/String/20)largest_substring_bw_two_equal_charcter_1624.cpp
@@ -38,3 +38,25 @@ int maxLengthBetweenEqualCharacters(string s) {
         }
         return ans;
 }
+
+
+
+Solution 3-:
+Steps-:
+1)just replace hashmap with count array size 26
+
+Code-: Time->O(N) Space->O(26)
+int maxLengthBetweenEqualCharacters(string s) {
+        int n=s.length();
+        int ans=-1;
+        vector<int>count(26,-1);
+        for(int i=0;i<n;i++)
+        {
+           char ch=s[i];
+           if(count[ch-'a']!=-1)
+           ans=max(ans,i-count[ch-'a']-1);
+           else
+           count[ch-'a']=i;
+        }
+        return ans;
+}
