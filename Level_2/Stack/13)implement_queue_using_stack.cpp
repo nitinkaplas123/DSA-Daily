04a01a4df9a35a8cb36ebd77916311a54dbafab4
unknown <nitinkaplas90643@gmail.com>
2024-01-30 00:19:59 +0000

    add

--- Level_2/Stack/13)implement_queue_using_stack.cpp
+++ Level_2/Stack/13)implement_queue_using_stack.cpp
@@ -1,27 +1,142 @@
-class Queue {
-    stack<int>s1;
-    stack<int>s2;
+Solution 1-:
+Steps-:
+1)Here we use 2 stacks s1 and s2.
+
+Pushing -:
+1)We do normally for push the element into the st1.
+
+Pop-:
+1)here we first put all the elements of st1 into st2.
+2)Then save the top element of st2 and the pop it.
+3)here that save element is our answer.
+
+Peek element-:
+1)same steps as we do in pop.
+
+isEmpty()-:
+1)return (st1.empty()==true);
+
+
+
+Code-:
+Time Complexity -:
+1) push ->O(1)
+2) pop  ->O(N)
+3) peek ->O(N)
+4) empty ->O(1)
+
+
+class MyQueue {
 public:
-
-    void enqueue(int x) {
-        s1.push(x);
+    MyQueue() {
+        
     }
-
-    int dequeue() {
-        if(s1.empty()==true) return -1;
-        
-        while(s1.size()>1)
+    stack<int>st1;
+    stack<int>st2;
+    void push(int x) {
+        st1.push(x);
+    }
+    
+    int pop() {
+        while(st1.empty()==false)
         {
-            s2.push(s1.top());
-            s1.pop();
+            int top=st1.top();
+            st1.pop();
+            st2.push(top);
         }
-        int ans=s1.top();
-        s1.pop();
-        
-        while(s2.empty()==false)
+
+        if(st2.empty()==true) return -1;
+        else 
         {
-            s1.push(s2.top());
-            s2.pop();
+           int ans=st2.top();
+           st2.pop();
+           while(st2.empty()==false)
+           {
+               int top=st2.top();
+               st2.pop();
+               st1.push(top);
+           }
+           return ans;
+        }
+    }
+    
+    int peek() {
+        if(st1.empty()==true) return -1;
+
+        while(st1.empty()==false)
+        {
+            st2.push(st1.top());
+            st1.pop();
+        }
+
+        int ans=st2.top();
+        while(st2.empty()==false)
+        {
+            st1.push(st2.top());
+            st2.pop();
         }
         return ans;
     }
+    
+    bool empty() {
+        return (st1.empty()==true);
+    }
+};
+
+
+
+
+Solution 2-:
+Steps-:
+1)Using 2 stack.
+
+Push-:
+1)here we use 2 stack st1,st2. 
+  where st1 is in the form of queue just do the dry run part for it.
+
+2)Pop-:
+  Simple top element of st1 is the pop.
+
+3)Peek-:
+  Simple top element of st1 is the peek.
+
+
+
+Code-:
+class MyQueue {
+public:
+    MyQueue() {
+        
+    }
+    stack<int>st1;
+    stack<int>st2;
+    
+    void push(int x) {
+        while(st1.empty()==false)
+        {
+            st2.push(st1.top());
+            st1.pop();
+        }
+        st1.push(x);
+
+        while(st2.empty()==false)
+        {
+            st1.push(st2.top());
+            st2.pop();
+        }
+    }
+    
+    int pop() {
+       int ans=st1.top();
+       st1.pop();
+       return ans;
+    }
+    
+    int peek() {
+        return st1.top();
+    }
+    
+    bool empty() {
+        return (st1.empty()==true);
+    }
+};
