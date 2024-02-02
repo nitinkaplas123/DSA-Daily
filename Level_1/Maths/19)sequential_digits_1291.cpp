915264971ce45f80fc19d34eee20a2ff8c43333f
unknown <nitinkaplas90643@gmail.com>
2024-02-02 13:29:08 +0000

    add

--- Level_1/Maths/19)sequential_digits_1291.cpp
+++ Level_1/Maths/19)sequential_digits_1291.cpp
@@ -39,7 +39,7 @@ Code-: Time->O(N^2)
 
 
 
-Solution 2-:
+Solution 2-: Using BFS Approach
 Steps-:
 1)here we are using queue 
 2)firstly we put all the numbers from 1 to 8 
@@ -95,6 +95,43 @@ vector<int> sequentialDigits(int low, int high) {
                val+=ld+1;
                q.push(val);
             }
+        }
+        return ans;
+    }
+
+
+
+
+Solution 3-:
+Steps-:
+1)Make a vector where we put all the possible numbers in it.
+2)Now traverse the vector v 
+  1. if v[i] is in the range then put it into vector v.
+  2. if v[i]>high break the loop.
+  3. if v[i]<low go to next value ->continue;
+
+
+Code-: Time->O(1) Space->O(1)
+vector<int> sequentialDigits(int low, int high) {
+        vector<int>ans;
+        vector<int>v={12,23,34,45,56,67,78,89,
+                      123,234,345,456,567,678,789,
+                      1234,2345,3456,4567,5678,6789,
+                      12345,23456,34567,45678,56789,
+                      123456,234567,345678,456789,
+                      1234567,2345678,3456789,
+                      12345678,23456789,
+                      123456789};
+
+        int n=v.size();
+        for(int i=0;i<n;i++)
+        {
+            if(v[i]>=low and v[i]<=high)
+            ans.push_back(v[i]);
+            else if(v[i]>high)
+            break;
+            else
+            continue;
         }
         return ans;
     }
